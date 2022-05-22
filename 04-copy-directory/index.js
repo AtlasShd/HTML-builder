361116f66228cd01e0b24cc350e70f3cb6920444
AtlasShd <tlsshrugged@gmail.com>
2022-05-22 19:02:34 +0300

    refactor: refactor 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,25 @@
-const fs = require('fs/promises');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 const SOURCE_FOLDER = path.resolve(__dirname, 'files');
 const DEST_FOLDER = path.resolve(__dirname, 'files-copy');
 
-(async () => {
+const copyDirectory = async () => {
   try {
-    await fs.mkdir(DEST_FOLDER, {recursive: true});
+    await fsPromises.mkdir(DEST_FOLDER, {recursive: true});
 
-    const listOfFiles = await fs.readdir(SOURCE_FOLDER);
+    const listOfFiles = await fsPromises.readdir(SOURCE_FOLDER);
 
     for (const file of listOfFiles) {
       const sourceFilePath = path.resolve(SOURCE_FOLDER, file);
       const destFilePath = path.resolve(DEST_FOLDER, file);
 
-      await fs.copyFile(sourceFilePath, destFilePath);
+      await fsPromises.copyFile(sourceFilePath, destFilePath);
     }
 
   } catch(e) {
     console.error(e);
   }
-})();
+};
+
+copyDirectory();
