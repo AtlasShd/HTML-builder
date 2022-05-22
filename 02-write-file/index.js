9b8cb53e2c66bcc35f1e4a181f0b12d756db7809
AtlasShd <tlsshrugged@gmail.com>
2022-05-22 18:40:38 +0300

    refactor: refactor 02-write-line task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,30 +3,37 @@ const path = require('path');
 const readline = require('readline');
 
 const STOP_WORD = 'exit';
-const FILE_NAME = 'text.txt';
+const FILE_NAME = path.resolve(__dirname, 'text.txt');
 
-const readConsole = readline.createInterface({
-  input: process.stdin, 
-  output: process.stdout
-});
+const writeFile = () => {
+  const readConsole = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+  });
+   
+  const writableStream = fs.createWriteStream(FILE_NAME);
+  writableStream.on('error', (error) => {
+    console.log(error);
+  });
 
-const writableStream = fs.createWriteStream(path.resolve(__dirname, FILE_NAME));
-writableStream.on('error', (error) => {
-  console.log(error);
-});
+  const writeLine = (text) => {
+    console.log('Anything else?');
 
-const writeLine = (text) => {
-  const lowerCaseText = text.toLowerCase().trim();
-  if (lowerCaseText === STOP_WORD) {
-    readConsole.close();
-  }
-  writableStream.write(String(text));
+    const lowerCaseText = text.toLowerCase().trim();
+    if (lowerCaseText === STOP_WORD) {
+      readConsole.close();
+    }
+
+    writableStream.write(String(text));
+  };
+  
+  const printLastMessage = () => {
+    console.log('Nice to meet you! Go-o-o-dby-y-e');
+  };
+  
+  readConsole.question(`Hello, ${process.env.USERNAME || 'User'}! Do you wanna tell me something interesting?\n`, writeLine);
+  readConsole.on('line', writeLine);
+  readConsole.on('close', printLastMessage);
 };
 
-const printLastMessage = () => {
-  console.log('Nice to meet you! Go-o-o-dby-y-e');
-};
-
-readConsole.question(`Hello, ${process.env.USERNAME}! Do you wanna tell me something interesting?\n`, writeLine);
-readConsole.on('line', writeLine);
-readConsole.on('close', printLastMessage);
+writeFile();
