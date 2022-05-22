adce13855cb6157c5417cc57bb10deeb2f861bea
AtlasShd <tlsshrugged@gmail.com>
2022-05-22 19:42:57 +0300

    refactor: refactor 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const SOURCE_FOLDER = path.resolve(__dirname, 'styles');
 const DEST_FILE = path.resolve(__dirname, 'project-dist/bundle.css');
 
-(async () => {
+const mergeStyles = async () => {
   try {
     const writableStream = fs.createWriteStream(DEST_FILE);
-    writableStream.on('error', (error) => {
-      console.error(`Error: ${error}`);
+    writableStream.on('error', (err) => {
+      console.error(err);
     });
 
     const listOfFiles = await fsPromises.readdir(SOURCE_FOLDER, {withFileTypes: true});
@@ -20,11 +20,18 @@ const DEST_FILE = path.resolve(__dirname, 'project-dist/bundle.css');
       }
 
       const readStream = fs.createReadStream(path.resolve(SOURCE_FOLDER, file.name), {encoding: 'utf-8'});
+      readStream.on('error', (err) => {
+        console.error(err);
+      });
+
       readStream.on('data', (chunk) => {
         writableStream.write(chunk);
       });
     }
+    console.log('Styles have been collected!');
   } catch(e) {
     console.error(e);
   }
-})();
+};
+
+mergeStyles();
