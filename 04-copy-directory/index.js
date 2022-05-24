66b14e1047f41ec0b7c02d02051e5344c5e4325a
AtlasShd <tlsshrugged@gmail.com>
2022-05-24 17:18:34 +0300

    refactor: refactor 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,34 @@ const DEST_FOLDER = path.resolve(__dirname, 'files-copy');
 
 const copyDirectory = async () => {
   try {
+    const recursiveRm = async (dirPath) => {
+      const files = await fsPromises.readdir(dirPath, {withFileTypes: true});
+
+      for (const file of files) {
+          if (file.isFile()) {
+            await fsPromises.unlink(path.resolve(dirPath, file.name));
+          } else {
+            await recursiveRm(path.resolve(dirPath, file.name));
+            await fsPromises.rmdir(path.resolve(dirPath, file.name));
+          }
+      }
+    };
+
+    const coreDir = await fsPromises.readdir(path.dirname(DEST_FOLDER), {withFileTypes: true});
+
+    for (const file of coreDir) {
+      if (file.name !== path.basename(DEST_FOLDER)) {
+        continue;
+      }
+      
+      if (file.isDirectory()) {
+        await recursiveRm(DEST_FOLDER);
+      } else {
+        console.error(`I can\'n create folder with name ${path.basename(DEST_FOLDER)} because file with this name already exist!`);
+      }
+    }
+    // Всё что выше - процесс актуализации
+
     await fsPromises.mkdir(DEST_FOLDER, {recursive: true});
 
     const listOfFiles = await fsPromises.readdir(SOURCE_FOLDER);
